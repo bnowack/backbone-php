60c9568d92d90aa9293f89e27e9e1992559f2a73
Benjamin Nowack <mail@bnowack.de>
2015-06-25 14:01:41 +0200

    Add initial index file

---
+++ src/Phamous/index.php
@@ -0,0 +1,10 @@
+<?php
+
+// base directory, derived from `/vendor/bnowack/phamous/src/Phamous/index.php`
+$baseDir = dirname(dirname(dirname(dirname(dirname(__DIR__))))) . '/';
+
+// include `vendor/autoload.php`
+require_once $baseDir . 'vendor/autoload.php';
+
+// create and start the application
+// ...
