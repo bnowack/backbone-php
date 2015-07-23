dd04daf1170962ad7b40bdd474fd08eda3eeaf31
Benjamin Nowack <mail@bnowack.de>
2015-07-23 16:57:34 +0200

    Create behat context skeletons (#setup)

---
+++ features/_bootstrap/UiContext.php
@@ -0,0 +1,28 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+
+use Behat\Behat\Tester\Exception\PendingException;
+
+use PHPUnit_Framework_TestCase as Assertions;
+
+use Phamous\Behat\ProjectContext;
+
+/**
+ * Defines application features from the specific context.
+ */
+class UiContext extends ProjectContext implements Context, SnippetAcceptingContext
+{
+
+    /**
+     * Initializes context.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+ 
+}
