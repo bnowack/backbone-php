183147a1a74496577697206d9476f60b15f221ce
Benjamin Nowack <mail@bnowack.de>
2015-11-10 12:52:25 +0100

    Extend test web server router script (#setup, #testing)

--- test/router.php
+++ test/router.php
@@ -20,5 +20,6 @@ if (preg_match('/\.(png|jpg|jpe?g|gif|css|js|json|txt)$/i', $_SERVER["REQUEST_UR
     return false;
 } else {
     (new BackbonePhp\Application())
+        ->setConfig('fileBase', dirname(__DIR__) . '/')
     ;
 }
