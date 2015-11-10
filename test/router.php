fd1bfe18e2acd9ff1fdb64ebccc143a07e3ef776
Benjamin Nowack <mail@bnowack.de>
2015-11-10 10:15:51 +0100

    Improve router script of the test web server (#testing)

--- test/router.php
+++ test/router.php
@@ -1,8 +1,15 @@
 <?php
 
+/**
+ * Sample router script for PHP's command-line server.
+ * 
+ * This router is for testing sites and applications using BackbonePHP with Behat.
+ * 
+ */
+
 // don't allow access to the router script during production
 if (php_sapi_name() !== 'cli-server') {
-    throw new Exception('The router script is only available during development');
+    die('The router script is only available on the test server');
 }
 
 // the base directory is 2 hops up from /test/router.php
