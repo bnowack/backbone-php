209a6991223cb430edd88e315a881a4bd2417679
Benjamin Nowack <mail@bnowack.de>
2015-11-10 13:17:07 +0100

    Add support for loading JSON files to config class (#dev, #config)

--- src/BackbonePhp/Config.php
+++ src/BackbonePhp/Config.php
@@ -38,4 +38,23 @@ class Config
        return isset($this->data[$name]) ? $this->data[$name] : null;
     }
     
+    /**
+     * Loads and applies configuration data from a (JSON) file
+     * 
+     * @param string $path Path to configuration file
+     * @param array $mergeFields config options that should be merged, not replaced during `Config::load`
+     * @return \BackbonePhp\Config 
+     */
+    public function load($path, $mergeFields = array())
+    {
+        foreach (File::loadJson($path) as $key => $value) {
+            if (in_array($key, $mergeFields) && isset($this->data[$key])) {
+                $this->data[$key] = array_merge($this->data[$key], $value);
+            } else {
+                $this->data[$key] = $value;
+            }
+        }
+        return $this;
+    }
+
 }
--- test/spec/BackbonePhp/ConfigSpec.php
+++ test/spec/BackbonePhp/ConfigSpec.php
@@ -7,6 +7,10 @@ use PhpSpec\ObjectBehavior;
 class ConfigSpec extends ObjectBehavior
 {
 
+    protected static function fixturesPath() {
+        return __DIR__ . '/../../fixtures/';
+    }
+    
     public function it_is_initializable()
     {
         $this->shouldHaveType('BackbonePhp\Config');
@@ -17,6 +21,25 @@ class ConfigSpec extends ObjectBehavior
         $this->set('foo', 'bar')->shouldReturn($this);
         $this->get('foo')->shouldReturn('bar');
         $this->get('baz')->shouldReturn(null);
+    }
+    
+    public function it_loads_options_from_json()
+    {
+        $this->load(self::fixturesPath() . 'config-1.json')->shouldReturn($this);
+        $this->get('foo')->shouldReturn('bar');
+        $this->get('baz')->shouldReturn(null);
+        $this->get('permissions')->shouldHaveCount(1);
+    }
+    
+    public function it_combines_loaded_config_options()
+    {
+        $this->load(self::fixturesPath() . 'config-1.json', array('permissions'))->shouldReturn($this);
+        $this->load(self::fixturesPath() . 'config-2.json', array('permissions'))->shouldReturn($this);
+        $this->get('foo')->shouldReturn('baz');
+        $this->get('baz')->shouldReturn(null);
+        $this->get('bat')->shouldReturn('test');
+        $this->get('permissions')->shouldBeArray();
+        $this->get('permissions')->shouldHaveCount(2);
     }
     
 }
