deda59d296f27c15424c7986d3ca72dc9a4386a4
Benjamin Nowack <mail@bnowack.de>
2015-12-03 08:59:39 +0100

    Create initial Response spec (#dev)

---
+++ test/spec/BackbonePhp/ResponseSpec.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace spec\BackbonePhp;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ResponseSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('BackbonePhp\Response');
+    }
+    
+    function it_sets_and_gets_a_header()
+    {
+        $this->setHeader('Content-Type', 'application/json')->shouldReturn($this);
+        $this->getHeader('Content-Type')->shouldReturn('application/json');
+    }
+    
+    function it_finds_a_header_case_insensitively()
+    {
+        $this->setHeader('Content-type', 'application/json')->shouldReturn($this);
+        $this->getHeader('conTent-TypE')->shouldReturn('application/json');
+    }
+    
+    public function it_sets_a_code() {
+        $this->setCode(200)->shouldReturn($this);
+        $this->getCode()->shouldReturn(200);
+    }
+    
+    public function it_is_successful_for_2xx_codes()
+    {
+        $this->setCode(200);
+        $this->isSuccessful()->shouldReturn(true);
+        
+        $this->setCode(400);
+        $this->isSuccessful()->shouldReturn(false);
+    }
+    
+    function it_sets_and_gets_the_body()
+    {
+        // text
+        $this->setBody('test')->shouldReturn($this);
+        $this->getBody()->shouldReturn('test');
+        // struct
+        $struct = array('te' => 'st');
+        $this->setBody($struct)->shouldReturn($this);
+        $this->getBody()->shouldReturn($struct);
+    }
+    
+    public function it_sends_headers()
+    {
+        // not fully testable at spec level
+        $this->setHeader('Content-Type', 'application/json')->shouldReturn($this);
+        $this->sendHeaders()->shouldReturn($this);
+    }
+    
+    public function it_sends_a_body()
+    {
+        $struct = array('test' => 'test');
+        $expected = json_encode($struct);
+        $this->setBody($struct)->shouldReturn($this);
+        ob_start(function($actual) use ($expected) {
+            if ($actual !== $expected) {
+                throw new \Exception('should send correct body (' . $actual . ' !== ' . $expected . ')');
+            }
+            return null;
+        });
+        $this->sendBody()->shouldReturn($this);
+        ob_end_flush();
+    }
+    
+}
