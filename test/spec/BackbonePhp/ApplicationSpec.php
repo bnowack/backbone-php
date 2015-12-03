8884f3c26057bf1600608564061dedc43602997c
Benjamin Nowack <mail@bnowack.de>
2015-12-03 08:54:15 +0100

    Improve spec method wording (#dev #documentation)

--- test/spec/BackbonePhp/ApplicationSpec.php
+++ test/spec/BackbonePhp/ApplicationSpec.php
@@ -17,13 +17,13 @@ class ApplicationSpec extends ObjectBehavior
         $this->shouldHaveType('BackbonePhp\Application');
     }
     
-    public function it_should_set_and_get_a_config_option()
+    public function it_sets_and_gets_a_config_option()
     {
         $this->setConfig('test', 'Test')->shouldReturn($this);
         $this->getConfig('test')->shouldReturn('Test');
     }
     
-    public function it_should_normalize_base_config_options()
+    public function it_normalizes_base_config_options()
     {
         // trailing slash on fileBase
         $this->setConfig('fileBase', 'path/to/dir')->getConfig('fileBase')->shouldReturn('path/to/dir/');
@@ -34,14 +34,14 @@ class ApplicationSpec extends ObjectBehavior
         $this->setConfig('webBase', 'path/from/root')->getConfig('webBase')->shouldReturn('path/from/root/');
     }
         
-    public function it_should_load_a_config_file()
+    public function it_loads_a_config_file()
     {
         $this->setConfig('fileBase', self::testsPath());
         $this->loadConfig('fixtures/config-1.json')->shouldReturn($this);
         $this->getConfig('foo')->shouldReturn('bar');
     }
     
-    function it_should_throw_an_exception_when_loading_a_non_existing_or_invalid_config_file()
+    function it_throws_an_exception_when_loading_a_non_existing_or_invalid_config_file()
     {
         $this->setConfig('fileBase', self::testsPath());
         $this->shouldThrow('\BackbonePhp\Exception\FileNotFoundException')->duringLoadConfig('fixtures/does-not-exist.json');
