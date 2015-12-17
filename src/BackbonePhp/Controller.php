a357e8dc26f27727c1fe46965964af80ab533319
Benjamin Nowack <mail@bnowack.de>
2015-12-17 10:20:39 +0100

    Implement basic Controller with template handler (#dev)

---
+++ src/BackbonePhp/Controller.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace BackbonePhp;
+
+/**
+ * BackbonePHP Controller Class
+ */
+class Controller
+{
+
+    /**
+     * @var Config Configuration object  
+     */
+    public $config;
+    
+    /**
+     * Instantiates the controller
+     * 
+     * @param Config $config Configuration object
+     */
+    public function __construct(Config $config = null)
+    {
+        $this->config = $config ?: new Config();
+    }
+    
+    /**
+     * Processes a basic template route request
+     * 
+     * Renders a page template with the body template specified in the route config, sets the title etc.
+     * 
+     * @param \BackbonePhp\Request $request Request
+     * @param \BackbonePhp\Response $response Response
+     * @param \stdClass $route Route object
+     * @return \BackbonePhp\Controller Controller instance
+     */
+    public function handleTemplateRouteRequest($request, $response, $route)
+    {
+        $pageTemplate = $route->model->pageTemplate;
+        $pageTitle = $this->getFullPageTitle($route);
+        $bodyTemplate = $route->template;
+        $content = (new Template($this->config))
+            ->setContent("{{$pageTemplate}}")
+            ->set('path', $request->get('resourcePath'))
+            ->set('pageTitle', $pageTitle)
+            ->set('body', "{{$bodyTemplate}}")
+            ->render()
+            ->getContent()
+        ;
+        $response->setBody($content);
+        return $this;
+    }
+    
+    /**
+     * Generates a combined page title from configured app title and page title
+     * 
+     * @param \stdClass $route Route object
+     * @return string Combined page title
+     */
+    protected function getFullPageTitle($route)
+    {
+        $pageTitle = isset($route->model->pageTitle)
+            ? $route->model->pageTitle
+            : ''
+        ;
+        $appTitle = isset($route->model->appTitle)
+            ? $route->model->appTitle
+            : $this->config->get('appTitle', '')
+        ;
+        $titleGlue = $this->config->get('pageTitleDelimiter', ' - ');
+        return ($pageTitle && $appTitle)
+            ? $pageTitle . $titleGlue . $appTitle
+            : $pageTitle . $appTitle
+        ;
+    }
+
+}
---
+++ test/spec/BackbonePhp/ControllerSpec.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace spec\BackbonePhp;
+
+use PhpSpec\ObjectBehavior;
+
+use BackbonePhp\Config;
+use BackbonePhp\Request;
+use BackbonePhp\Response;
+
+use PHPUnit_Framework_TestCase as Assertions;
+
+class ControllerSpec extends ObjectBehavior
+{
+    
+    protected static function rootPath()
+    {
+        return dirname(dirname(dirname(__DIR__))) . '/';
+    }
+    
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType('BackbonePhp\Controller');
+    }
+    
+    public function it_has_a_config_object()
+    {
+        $this->config->shouldHaveType('BackbonePhp\Config');
+    }
+    
+    public function it_handles_a_template_request()
+    {
+        // config
+        $config = new Config();
+        $config->set('fileBase', self::rootPath());
+        $this->config = $config;
+        
+        $request = new Request();
+        $response = new Response();
+        
+        $config->set('appTitle', 'test');
+        $request->setPath('/test');
+        
+        $route = (object)[
+            "path" => "/test",
+            "type" => "template",
+            "template" => "/test/fixtures/static-body.html.tpl",
+            "model" => (object)[
+                "pageTemplate" => "/test/fixtures/index.html.tpl",
+                "pageTitle" => 'test'
+            ]
+        ];
+        $this->handleTemplateRouteRequest($request, $response, $route)->shouldReturn($this);
+    }
+    
+    public function it_adds_the_app_title_to_the_page_title()
+    {
+        // init
+        $config = new Config();
+        $config->set('fileBase', self::rootPath());
+        $this->config = $config;
+        
+        $request = new Request();
+        $response = new Response();
+        
+        // request with appended app title
+        $config->set('appTitle', 'app');
+        $request->setPath('/test');
+        
+        $route = (object)[
+            "path" => "/test",
+            "type" => "template",
+            "template" => "/test/fixtures/static-body.html.tpl",
+            "model" => (object)[
+                "pageTemplate" => "/test/fixtures/index.html.tpl",
+                "pageTitle" => 'page'
+            ]
+        ];
+        $this->handleTemplateRouteRequest($request, $response, $route);
+        $actual1 = $response->getBody();
+        $expected1 = '<title>page - app</title>';
+        Assertions::assertContains($expected1, $actual1, 'should have page title and app title');
+
+        // request with empty page title
+        $route->model->pageTitle = null;
+        $this->handleTemplateRouteRequest($request, $response, $route);
+        $actual2 = $response->getBody();
+        $expected2 = '<title>app</title>';
+        Assertions::assertContains($expected2, $actual2, 'should have app title only');
+
+        // request with empty app title
+        $route->model->pageTitle = 'page';
+        $config->set('appTitle', null);
+        $this->handleTemplateRouteRequest($request, $response, $route);
+        $actual3 = $response->getBody();
+        $expected3 = '<title>page</title>';
+        Assertions::assertContains($expected3, $actual3, 'should have page title only');
+    }
+    
+}
